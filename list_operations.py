1fecdc6f7349549d32d7eba7f299168e48c387cb
Hackbright Student <no-reply@hackbrightacademy.com>
2018-06-25 19:41:51 -0700

    All functions are done

--- list_operations.py
+++ list_operations.py
@@ -86,7 +86,7 @@ def first_three(input_list):
 
     """
 
-    return []
+    return input_list[:3]
 
 
 def last_five(input_list):
@@ -99,7 +99,7 @@ def last_five(input_list):
 
     """
 
-    return []
+    return input_list[-5:]
 
 
 def middle(input_list):
@@ -112,7 +112,7 @@ def middle(input_list):
 
     """
 
-    return []
+    return input_list[2:-2]
 
 
 def inner_four(input_list):
@@ -125,7 +125,7 @@ def inner_four(input_list):
 
     """
 
-    return []
+    return input_list[2:6]
 
 
 def inner_four_end(input_list):
@@ -141,7 +141,7 @@ def inner_four_end(input_list):
 
     """
 
-    return []
+    return input_list[-6:-2]
 
 
 def replace_head(input_list):
@@ -156,6 +156,7 @@ def replace_head(input_list):
 
     """
 
+    input_list[0] = 42
     pass
 
 
@@ -170,7 +171,8 @@ def replace_third_and_last(input_list):
     True
 
     """
-
+    input_list[2] =  37
+    input_list[-1] = 37
     pass
 
 
@@ -189,7 +191,7 @@ def replace_middle(input_list):
     True
 
     """
-
+    input_list[2:-2] = [42, 37]
     pass
 
 
@@ -204,7 +206,8 @@ def delete_third_and_seventh(input_list):
     True
 
     """
-
+    del input_list[2]
+    del input_list[6]
     pass
 
 
@@ -221,7 +224,7 @@ def delete_middle(input_list):
     True
 
     """
-
+    del input_list[2:-2]
     pass
 
 
